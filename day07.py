95c75a5f92d01c24beaf658b7de4fbc2eff31f6f
Cameron Spickert <cameron@checkhq.com>
2021-12-07 22:58:34 -0800

    Day 7 part 1

--- day07.py
+++ day07.py
@@ -3,10 +3,13 @@ from base import BaseSolution
 
 class Solution(BaseSolution):
     def load_data(self, input):
-        pass
+        return [int(value) for value in input.split(",")]
 
     def part1(self, data):
-        pass
+        return min(self.get_cost(data, position) for position in data)
 
     def part2(self, data):
         pass
+
+    def get_cost(self, data, position):
+        return sum(abs(data[i] - position) for i in range(len(data)))
