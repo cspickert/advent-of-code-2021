7663f755197a8458b6e8324c69f17866b5e9681d
Cameron Spickert <cameron@checkhq.com>
2021-12-07 21:59:31 -0800

    Day 6 part 1

--- day06.py
+++ day06.py
@@ -3,10 +3,22 @@ from base import BaseSolution
 
 class Solution(BaseSolution):
     def load_data(self, input):
-        pass
+        return [int(value) for value in input.split(",")]
 
     def part1(self, data):
-        pass
+        return self.simulate(data, 80)
 
     def part2(self, data):
+        # return self.simulate(data, 256)
         pass
+
+    def simulate(self, data, days):
+        for _ in range(days):
+            next_data = []
+            for i in range(len(data)):
+                if data[i] > 0:
+                    next_data.append(data[i] - 1)
+                else:
+                    next_data.extend((6, 8))
+            data = next_data
+        return len(data)
