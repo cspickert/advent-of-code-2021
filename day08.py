f7119f4a593a1e04394b2e0c2e8db22b3d77e53f
Cameron Spickert <cameron@checkhq.com>
2021-12-10 15:23:45 -0800

    Day 8 part 1

--- day08.py
+++ day08.py
@@ -3,10 +3,15 @@ from base import BaseSolution
 
 class Solution(BaseSolution):
     def load_data(self, input):
-        pass
+        return [
+            [section.split(" ") for section in line.split(" | ")]
+            for line in input.splitlines()
+        ]
 
     def part1(self, data):
-        pass
+        return sum(
+            len(output) in {2, 3, 4, 7} for _, outputs in data for output in outputs
+        )
 
     def part2(self, data):
         pass
