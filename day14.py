795ec843d0fecc6776a6e57684c9df945126432e
Cameron Spickert <cameron@checkhq.com>
2021-12-14 00:47:26 -0500

    Day 14 part 1

--- day14.py
+++ day14.py
@@ -1,12 +1,50 @@
+from collections import Counter
+
 from base import BaseSolution
 
 
 class Solution(BaseSolution):
     def load_data(self, input):
-        pass
+        input = """\
+NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C"""
+        value, mapping_inputs = input.split("\n\n")
+        mapping = dict(line.split(" -> ") for line in mapping_inputs.splitlines())
+        return value, mapping
 
     def part1(self, data):
-        pass
+        value, mapping = data
+        for _ in range(10):
+            value = self.step(value, mapping)
+        return self.analyze(value)
 
     def part2(self, data):
         pass
+
+    def step(self, value, mapping):
+        result = value[0]
+        for i in range(len(value)):
+            key = value[i - 1 : i + 1]
+            if key in mapping:
+                result += mapping[key] + value[i]
+        return result
+
+    def analyze(self, value):
+        counts = Counter(value)
+        return max(counts.values()) - min(counts.values())
