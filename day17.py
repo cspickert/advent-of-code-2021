a797397036ba845882f167f344d3a856419ad159
Cameron Spickert <cameron@checkhq.com>
2021-12-18 15:24:34 -0500

    Day 17 part 2

--- day17.py
+++ day17.py
@@ -1,3 +1,4 @@
+import itertools
 import re
 
 from base import BaseSolution
@@ -23,4 +24,62 @@ class Solution(BaseSolution):
         return sum(range(-target_area["y"][0]))
 
     def part2(self, target_area):
-        pass
+        x_velocities = self.find_x_velocities(target_area)
+        y_velocities = self.find_y_velocities(target_area)
+        return len(
+            [
+                (dx, dy)
+                for dx in x_velocities
+                for dy in y_velocities
+                if self.probe(target_area, dx, dy)
+            ]
+        )
+
+    def find_x_velocities(self, target_area):
+        x_range = target_area["x"]
+        x_velocities = []
+        for x_velocity in range(max(x_range), 0, -1):
+            x, dx = 0, x_velocity
+            while True:
+                if x in x_range:
+                    x_velocities.append(x_velocity)
+                    break
+                x += dx
+                dx -= 1
+                if dx < 0:
+                    break
+        return set(x_velocities)
+
+    def find_y_velocities(self, target_area):
+        y_range = target_area["y"]
+        y_velocities = []
+        for y_velocity in range(min(y_range), -min(y_range) + 1):
+            y, dy = 0, y_velocity
+            while True:
+                if y in y_range:
+                    y_velocities.append(y_velocity)
+                    break
+                y += dy
+                dy -= 1
+                if y < min(y_range):
+                    break
+        return set(y_velocities + [-dy for dy in y_velocities])
+
+    def probe(self, target_area, x_velocity, y_velocity):
+        x_range = target_area["x"]
+        y_range = target_area["y"]
+        x, y = 0, 0
+
+        valid_x_range = range(max(x_range))
+        valid_y_range = range(10 ** 10, min(y_range), -1)
+
+        while x in valid_x_range and y in valid_y_range:
+            x += x_velocity
+            y += y_velocity
+            if x_velocity > 0:
+                x_velocity -= 1
+            y_velocity -= 1
+            if x in x_range and y in y_range:
+                return True
+
+        return False
